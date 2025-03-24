962e8ba88430200dbecd559e9b1dcdc03ea18a8d
brian-tura <b.tura2002@gmail.com>
2025-03-24 11:27:40 +0100

    function created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function isPalindroma(parola){
+    check = parola.split("").reverse("").join("");
+    if(check === parola){
+        console.log("La parola è palindroma");
+    }
+    else{
+        console.log("La parola non è palindroma");
+    }
+}
 
 parola = prompt("inserire parola da verificare");
 isPalindroma(parola);
